b0aa126decb6e9a10d49c7590a4b6ace83fcf998
giovanef16 <giovane_16silva@hotmail.com>
2025-01-25 13:37:14 -0300

    Implementando página de Artigos

--- src/Providers/Post.php
+++ src/Providers/Post.php
@@ -7,6 +7,7 @@ use EquilibrioFinanceiro\Models\Post as PostModel;
 class Post
 {
     private $destaques;
+    private $artigos;
 
     public function getDestaques(): array
     {
@@ -27,5 +28,26 @@ class Post
         }, $posts);
 
         return $this->destaques;
+    }
+
+    public function getArtigos(): array
+    {
+        if (isset($this->artigos)) {
+            return $this->artigos;
+        }
+
+        $posts = get_posts([
+            "numberposts" => 10
+        ]);
+
+        if (!$posts) {
+            return [];
+        }
+
+        $this->artigos = array_map(function ($post) {
+            return new PostModel($post);
+        }, $posts);
+
+        return $this->artigos;
     }
 }
